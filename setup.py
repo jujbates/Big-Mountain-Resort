aee139c6f27ac7ab5f238c6ed13e13e744c8d366
Justin Bates <jujbates@gmail.com>
2020-08-10 12:39:33 -0700

    reorganized the repo with cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='How much will Big Mountain Resort need to increase their weekday and weekend adult lift tickets to in order to maintain a minimum profit margin of 9.2% for its investors during the upcoming season, while covering an additional operating costs of $1,540,000 for new chair lifts?',
+    author='Justin Bates',
+    license='MIT',
+)
